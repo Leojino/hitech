6b526078b7160609aa0e7996007d199677786756
Jino John <leojino@gmail.com>
2018-02-10 01:40:41 +0530

    fix relative path

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-import Home from './components/Home'
+import Home from './Home';
 
 class App extends Component {
   render() {
