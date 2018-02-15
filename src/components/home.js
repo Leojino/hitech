01062d4d7841cdba37fb00ed9214083028674b09
Jino John <leojino@gmail.com>
2018-02-16 01:16:27 +0530

    add carousel

--- src/components/home.js
+++ src/components/home.js
@@ -9,14 +9,36 @@ class Home extends Component {
     return (
       <div className="wrapper">
         <Header/>
+
+        <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active" key={21}>
+            <img className="d-block w-100" src="../images/1.jpeg" alt="First slide"/>
+          </div>
+          <div className="carousel-item" key={23}>
+            <img className="d-block w-100" src="../images/3.jpeg" alt="Third slide"/>
+          </div>
+        </div>
+      </div>
+
+        {/* <div className="jumbotron">
+          <h1 className="display-4">Hello, world!</h1>
+          <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+          <hr className="my-4"/>
+          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+          <p className="lead">
+            <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+          </p>
+        </div> */}
+
         <div className="container">
           <div className="row">
           {tileData.map(value => (
-                <div key={value.code} className="col-xs-12 col-sm-6 col-md-4">
+                <div key={value.code} className="col-xs-12 col-sm-6 col-md-4" style={{marginBottom: "20px"}}>
                   <div className="card">
                     <div className="card-body">
                       <h5 className="card-title">{value.name}</h5>
-                      <p className="card-text">Some</p>
+                      <p className="card-text">{value.range}</p>
                     </div>
                   </div>
                 </div>
--- src/components/tileData.js
+++ src/components/tileData.js
@@ -1,15 +1,28 @@
 export default [{
-    name: "variac 1",
-    price: 2000,
+    name: "DL (R) Variac",
+    range: ["2ADL - 22 ADL at 230V, 165V - 275V"],
     code: 1
 },
 {
-    name: "variac 2",
-    price: 2000,
+    name: "Isolated Variac",
+    range: [
+            "Input - 230V Output - 0 to 230V @ 2 Amp, 4 Amp, 10 Amp, 23 Amp",
+            "Input 230V Output 0 to 15V / 25V / 35V @ 6 Amp, 8 Amp, 10 Amp, 20 Amp"
+            ],
     code: 2
 },
 {
-    name: "variac 3",
-    price: 2000,
+    name: "Single Layer Variac",
+    range: [".75 amp, 0-270 V, 2 amps to 35 amp, 0-270 V AC Motorised Version 400Hz Models"],
     code: 3
+},
+{
+    name: "DL (N) Variac",
+    range: ["2 ADL - 22 ADL at 230 V", "165V - 275V"],
+    code: 4
+},
+{
+    name: "Torrodial Variac",
+    range: ["Custom Build: 30VA to 4 KVA"],
+    code: 5
 }]
